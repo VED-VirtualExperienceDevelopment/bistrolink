e2707515addc250e1240a6272a340752b0e05c8b
Daiana Silvera <daiana.silvera@estudiantes.utec.edu.uy>
2026-06-24 21:57:34 -0300

    ci(BL-22): configuracion grafana loki

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HealthController } from './health.controller'
+import { HealthController } from './health.controller';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-loki',
+          options: {
+            batching: true,
+            interval: 5,
+            host: `${process.env.LOKI_URL}`,
+            basicAuth: {
+              username: process.env.LOKI_USERNAME,
+              password: process.env.LOKI_PASSWORD,
+            },
+            labels: {
+              app: 'bistrolink',
+              env: process.env.NODE_ENV ?? 'staging',
+            },
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,17 +1,14 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common'; // <-- Aquí está definido
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  // 1. Instanciamos el Logger pasándole el contexto 'Main' para usarlo
-  const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   app.enableCors();
-
   const port = process.env.PORT || 3001;
   await app.listen(port);
-  // 2. Aquí lo usamos. Al llamarlo, ESLint verá que ya no está en desuso
-  logger.log(`Application is running on: http://localhost:${port}`);
+  app.get(Logger).log(`Application is running on: http://localhost:${port}`, 'Main');
 }
 bootstrap();
